bcd4b233554fe2f33e424983b34c9efc6ba0e439
Luciano Junior <bragajuniordev@gmail.com>
2025-06-29 23:15:39 -0300

    tasks funcionando

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Task
+from . import db
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
     lista_de_tarefas = Task.query.all()
-    return render_template('home.html', task=lista_de_tarefas)
+    return render_template('home.html', tasks=lista_de_tarefas)
+
+@main.route('/add', methods=['POST'])
+def add_task():
+    text_form = request.form['content']
+    nova_tarefa = Task(content=text_form)
+    db.session.add(nova_tarefa)
+    db.session.commit()
+    return redirect(url_for('main.home'))
+    
